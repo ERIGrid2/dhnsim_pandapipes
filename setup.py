06a0f5303f20444cebc643f9cecd050e4e8d832d
cwowi <cwowi@elektro.dtu.dk>
2021-07-19 17:20:07 +0200

    Created steup.py, updated LICENSE file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dh_network_simulator',
+    version='0.1.0',
+    packages=['test', 'test.io', 'test.component_models', 'dh_network_simulator', 'dh_network_simulator.io', 'dh_network_simulator.component_models'],
+    url='https://github.com/cwowi/dh_network_simulator',
+    license='BSD 3-Clause License',
+    author='Christopher W. Wild',
+    author_email='cwowi@elektro.dtu.dk',
+    description=''
+)
